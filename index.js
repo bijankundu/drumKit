2ba23a395de1f1d75fd5d9ec47e364c6404e45e3
Bijan Kundu <bijankundu025@gmail.com>
2019-12-25 21:59:46 +0530

    fixed sound path

--- index.js
+++ index.js
@@ -16,27 +16,27 @@ function makeSound(key)
               audio.play();
               break;
     case 'a':
-              audio = new Audio('../sounds/kick-bass .mp3');
+              audio = new Audio('sounds/kick-bass .mp3');
               audio.play();
               break;
     case 's':
-              audio = new Audio('../sounds/snare.mp3');
+              audio = new Audio('sounds/snare.mp3');
               audio.play();
               break;
     case 'd':
-              audio = new Audio('../sounds/tom-1.mp3');
+              audio = new Audio('sounds/tom-1.mp3');
               audio.play();
               break;
     case 'j':
-              audio = new Audio('../sounds/tom-2.mp3');
+              audio = new Audio('sounds/tom-2.mp3');
               audio.play();
               break;
     case 'k':
-              audio = new Audio('../sounds/tom-3.mp3');
+              audio = new Audio('sounds/tom-3.mp3');
               audio.play();
               break;
     case 'l':
-              audio = new Audio('../sounds/tom-4.mp3');
+              audio = new Audio('sounds/tom-4.mp3');
               audio.play();
               break;
     default: console.log(key);
